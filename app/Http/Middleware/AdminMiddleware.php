425c760047ad904389bda38db89af72b6e05e466
mariortega97 <avemario32@gmail.com>
2019-02-11 13:51:52 +0100

    Añadido Middleware para controlar el acceso a las zonas de Administrador

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -16,7 +16,7 @@ class AdminMiddleware
      */
     public function handle($request, Closure $next, $admin)
     {
-        if (! $request->user()->hasRole($admin)) {
+        if ($request->user()->admin!=$admin) {
             return redirect('/');
             }
         return $next($request);
