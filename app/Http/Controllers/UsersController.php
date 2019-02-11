a75e7f11f9380aa5b0dc654c99558ba91f69135a
jtorsa <jtorsadaw@gmail.com>
2019-02-11 20:40:56 +0100

    comentado showOrders

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,10 +9,10 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function showOrders($id){
+    /*public function showOrders($id){
         $usuario = User::where('id', $id)->first();
         return view('user.orders',  array('user' => $usuario));
-    }
+    }*/
 
     public function showAdminPanel(){
         return view('user.admin.index');
