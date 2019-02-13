f6e52f8410ebbfeb5208bd549b3e8bef98c8d79b
jtorsa <jtorsadaw@gmail.com>
2019-02-13 17:54:14 +0100

    carousel cambia

--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -1,5 +1,5 @@
 <!----------------       CAROUSEL       ---------------->
-<div class="carousel slide" id="carousel-16552">
+<div class="carousel slide" id="carousel-16552" data-ride="carousel" data-interval="4000">
     <ol class="carousel-indicators">
         <li data-slide-to="0" data-target="#carousel-16552" class="active"></li>
         <li data-slide-to="1" data-target="#carousel-16552"></li>
@@ -14,14 +14,14 @@
             </div>
         </div>
         <div class="carousel-item">
-        <img class="d-block img-responsive" alt="Carousel Bootstrap Second" src="img/oferta1.jpg"  height="150" width="100" >
+        <img class="d-block img-responsive" alt="Carousel Bootstrap Second" src="img/oferta4.jpg"  height="150" width="100" >
             <div class="carousel-caption">
                 <h4>Second Thumbnail label</h4>
                 <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
             </div>
         </div>
         <div class="carousel-item">
-            <img class="d-block img-responsive" width="100" height="150" alt="Carousel Bootstrap Third" src="img/oferta1.jpg">
+            <img class="d-block img-responsive" width="100" height="150" alt="Carousel Bootstrap Third" src="img/oferta6.jpg">
             <div class="carousel-caption">
                 <h4>Third Thumbnail label</h4>
                 <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
